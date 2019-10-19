87ec864fcc248971f27c4b15d3b1685bdc8484c0
Alisher <shera.rashiduulu@gmail.com>
2019-10-19 13:12:25 +0600

    создао forms.py для Poll, Choice

---
+++ source/webapp/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from webapp.models import Poll, Choice
+
+
+class PollForm(forms.ModelForm):
+    class Meta:
+        model = Poll
+        fields = ['question']
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Choice
+        fields = ['text', 'poll']
+
+
+
+class SimpleSearchForm(forms.Form):
+    search = forms.CharField(max_length=100, required=False, label='Search')
--- source/webapp/views/poll_views.py
+++ source/webapp/views/poll_views.py
@@ -18,7 +18,34 @@ class IndexView(ListView):
 
 
 
-class TaskView(DetailView):
+class PollView(DetailView):
     template_name = 'poll/poll.html'
     context_object_name = 'poll'
     model = Poll
+
+
+class PollCreateView(CreateView):
+    model = Poll
+    template_name = 'poll/create.html'
+    form_class = PollForm
+
+    def get_success_url(self):
+        return reverse('poll_view', kwargs={'pk': self.object.pk})
+
+
+class PollUpdateView(UpdateView):
+    model = Poll
+    template_name = 'poll/update.html'
+    form_class = PollForm
+    context_object_name = 'poll'
+
+    def get_success_url(self):
+        return reverse('task_view', kwargs={'pk': self.object.pk})
+
+
+class TaskDeleteView(DeleteView):
+    model = Poll
+    template_name = 'poll/delete.html'
+    context_object_name = 'task'
+    success_url = reverse_lazy('index')
+
