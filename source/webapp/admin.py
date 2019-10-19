f68e420236dce5ec574726b1e4af3b22f50301cc
Alisher <shera.rashiduulu@gmail.com>
2019-10-19 12:47:34 +0600

    создал фикстуру

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from webapp.models import Poll, Choice
 
-# Register your models here.
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'question', 'created_at']
+    list_filter = ['question']
+    list_display_links = ['pk', 'question']
+    search_fields = ['question', 'created_at']
+    fields = ['question', 'created_at']
+    readonly_fields = ['created_at']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -4,10 +4,18 @@ from django.db import models
 class Poll(models.Model):
     question = models.TextField(max_length=200, null=False, blank=False, verbose_name='Вопрос')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
-    poll = models.ForeignKey('webapp.Choice', related_name='polls_choice',
-                             on_delete=models.PROTECT, null=False, blank=False, verbose_name='варианты')
+
+    def __str__(self):
+        return self.question
 
 
 class Choice(models.Model):
     text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
-    poll = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок')
+    poll = models.ForeignKey('webapp.Poll', related_name='polls_choice',
+                             on_delete=models.CASCADE, null=False, blank=False, verbose_name='Опрос')
+
+    def __str__(self):
+        return self.text
+
+
+
